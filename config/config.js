d8734168d3b9c3fa1943ec451b3e74c0e777edcc
the-poor-gamer-124 <46222936+the-poor-gamer-124@users.noreply.github.com>
2019-07-30 17:56:07 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {};
 
 config.port = process.env.PORT || 5000;
-config.dbURL = process.env.DATABASEURL || "mongodb://localhost/minicord";
+config.dbURL = process.env.DATABASEURL || "mongodb+srv://yeet:@cluster0-bxfke.mongodb.net/test?retryWrites=true&w=majority";
 
 module.exports = config;
